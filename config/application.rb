0610b8a2f8c5e239b7520cc877206ff4301e6fc1
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-03-16 23:31:05 +0900

    ホスト許可設定追加

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module MatsukenQuiz
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << "matsuken-quiz.onrender.com"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
