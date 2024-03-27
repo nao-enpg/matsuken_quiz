a24db7ddc88c45ad935b5cfc04c45eda57b140c5
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-03-27 22:33:48 +0900

    DB作成及びコード修正

--- app/controllers/sambas_controller.rb
+++ app/controllers/sambas_controller.rb
@@ -1,6 +1,4 @@
 class SambasController < ApplicationController
-  require 'rspotify'
-  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
   before_action :set_songs_options, only: [:index, :check_answer]
   before_action :set_random_song, only: [:index, :check_answer, :show_result]
 
@@ -21,26 +19,17 @@ class SambasController < ApplicationController
   private
 
   def set_songs_options
-    @songs_options = Samba::Samba_IDS.map do |track_id|
-      RSpotify::Track.find(track_id)
-    end
+    @songs_options = Samba.all
   end
 
   def set_random_song
     unless ['check_answer', 'show_result'].include?(action_name)
-      random_track_id = Samba::Samba_IDS.sample
-      @random_song = RSpotify::Track.find(random_track_id)
-      @preview_url = fetch_preview_url(@random_song) # 変更: @preview_urlインスタンス変数に格納
-      session[:random_song_id] = random_track_id
+      @random_song = Samba.order("RANDOM()").first
+      @preview_url = @random_song.preview_url
+      session[:random_song_id] = @random_song.id
     else
-      @random_song = RSpotify::Track.find(session[:random_song_id])
-      @preview_url = fetch_preview_url(@random_song) # 変更: @preview_urlインスタンス変数に格納
-    end
-  end
-
-  def fetch_preview_url(song)
-    Rails.cache.fetch("songs/#{song.id}/preview_url", expires_in: 2.months) do
-      song.preview_url
+      @random_song = Samba.find(session[:random_song_id])
+      @preview_url = @random_song.preview_url
     end
   end
 end
