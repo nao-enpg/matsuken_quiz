6944d079fbfc3631c098c5ec617c5f4d02b34f05
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-03-30 12:28:19 +0900

    ToSとPPをサンバ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/terms_of_service', as: 'terms_of_service'
+  get 'static_pages/privacy_policy', as: 'privacy_policy'
   root "sambas#index"
   resources :sambas, only: [:index] do
     collection do
